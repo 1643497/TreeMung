3abe2a35715c1bb3c37da475e0b645b357d5e3ba
82106 <82106@LAPTOP-U59N7E0O>
2025-12-30 21:03:41 +0900

    d

---
+++ TreeMung/src/main/java/com/TreeMung/www/TreeMungApplication.java
@@ -0,0 +1,13 @@
+package com.TreeMung.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeMungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeMungApplication.class, args);
+	}
+
+}
---
+++ TreeMung/src/test/java/com/TreeMung/www/TreeMungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TreeMung.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeMungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
